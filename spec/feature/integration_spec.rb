5227cee2244f3f2bdddca7dd55cf8ae8c5bfcb76
root <root@localhost>
2022-09-15 18:09:32 +0000

    workflows local save

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: 20
+    select '2017', from: 'book_published_date_1i'
+    select 'June', from: 'book_published_date_2i'
+    select '26', from: 'book_published_date_3i'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content(20)
+    expect(page).to have_content('2017-06-26')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter',
+                        author: 'J.K. Rowling',
+                        price: 20,
+                        published_date: Date.parse('2017-06-26','%Y-%m-%d'))
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +17,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a publication date' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+
 end
