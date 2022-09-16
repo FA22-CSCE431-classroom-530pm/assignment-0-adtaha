05f931ad1751a5d9001c586111cb4290cf2ecb77
adtaha <60298322+adtaha@users.noreply.github.com>
2022-09-15 20:57:24 -0500

    Please work

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   resources :books
   root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-=======
-Rails.application.routes.draw do
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
->>>>>>> cefba731e4ea9462c1f88bf89ce69686ecba59f9
+enabled
