800dcc71b5da44a4f0c4ec47f47d327b131a011a
adtaha <60298322+adtaha@users.noreply.github.com>
2022-09-15 21:22:46 -0500

    msdkdjsksjskjfkf

--- db/migrate/20220915163851_create_books.rb
+++ db/migrate/20220915163851_create_books.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:db/migrate/20220915163851_create_books.rb
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
@@ -11,14 +10,3 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     end
   end
 end
-=======
-class CreateBooks < ActiveRecord::Migration[6.1]
-  def change
-    create_table :books do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
->>>>>>> cefba731e4ea9462c1f88bf89ce69686ecba59f9:db/migrate/20220826171546_create_books.rb
